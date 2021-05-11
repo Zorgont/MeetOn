08d07fe496df62d190a03f75d79ff1bdda1a0c93
Zorgont <zorgont@gmail.com>
2021-05-11 15:47:48 +0300

    Fix bug in request controller.

--- MeetOnBackend/src/main/java/com/example/meetontest/controllers/RequestController.java
+++ MeetOnBackend/src/main/java/com/example/meetontest/controllers/RequestController.java
@@ -6,16 +6,19 @@ import com.example.meetontest.dto.MessageResponse;
 import com.example.meetontest.dto.RequestDTO;
 import com.example.meetontest.entities.Request;
 import com.example.meetontest.entities.RequestStatus;
+import com.example.meetontest.exceptions.ValidatorException;
 import com.example.meetontest.megabrainutils.CheckUserCompliance;
 import com.example.meetontest.services.MeetingService;
 import com.example.meetontest.services.RequestService;
 import com.example.meetontest.services.UserService;
+import com.example.meetontest.services.impl.UserDetailsImpl;
 import com.example.meetontest.validators.RequestValidator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -78,23 +81,23 @@ public class RequestController {
     @PutMapping("/changeStatus/{id}")
     public ResponseEntity<?> updateRequestStatus(@PathVariable Long id, @RequestParam String status) {
         try {
-                RequestDTO dto = requestConverter.convertBack(requestService.getById(id).get());
+            RequestDTO dto = requestConverter.convertBack(requestService.getById(id).get());
+
+            // Если менеджер митинга изменяет статус:
+            if (meetingService.getManager(meetingService.getMeetingById(dto.getMeeting_id())).getId().equals(((UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId())) {
                 dto.setStatus(status);
-            return requestController.updateRequestStatusWithDTO(dto) ? ResponseEntity.ok(dto) : ResponseEntity.badRequest().body(new MessageResponse("Wrong id!"));
+                requestService.changeStatus(requestService.getById(dto.getId()).get(), RequestStatus.valueOf(dto.getStatus().toUpperCase()));
+                return ResponseEntity.ok(requestConverter.convertBack(requestService.getById(dto.getId()).get()));
+            }
+            throw new ValidatorException("Wrong action!");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
         }
-    }
-    @CheckUserCompliance
-    public Boolean updateRequestStatusWithDTO(@DTO(name = "user_id") RequestDTO requestDTO) throws JsonProcessingException {
-        requestService.changeStatus(requestService.getById(requestDTO.getId()).get(), RequestStatus.valueOf(requestDTO.getStatus().toUpperCase()));
-        return true;
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> removeRequestById(@PathVariable Long id) {
         try {
-
             return requestController.deleteRequestStatusWithDTO(requestConverter.convertBack(requestService.getById(id).get())) ?
                     ResponseEntity.ok("deleted") : ResponseEntity.badRequest().body(new MessageResponse("Wrong id!"));
         } catch (Exception e) {
